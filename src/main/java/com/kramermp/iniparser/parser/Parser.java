2bb880bb7a20daa18a4a979875d31758d8cd5284
Michael <kramermp5@gmail.com>
2022-02-19 14:51:35 -0500

    Implemented is Key

--- src/main/java/com/kramermp/iniparser/parser/Parser.java
+++ src/main/java/com/kramermp/iniparser/parser/Parser.java
@@ -1,13 +1,19 @@
 package com.kramermp.iniparser.parser;
 
 public class Parser {
+    public static final char COMMENT_CHAR = '#';
+    public static final char KEY_OPENER = '[';
+    public static final char KEY_CLOSER = ']';
 
     public static boolean isComment(String testString) {
-        return testString != null && testString.length() > 0 && testString.charAt(0) == '#';
+        return testString != null && testString.length() > 0 && testString.charAt(0) == COMMENT_CHAR;
     }
 
     public static boolean isKey(String testString) {
-        return false;
+        return testString != null
+                && testString.length() > 0
+                && testString.charAt(0) == KEY_OPENER
+                && testString.charAt(testString.length() - 1) == KEY_CLOSER;
     }
 
     public static boolean isLine(String testString) {
--- src/test/java/com/kramermp/iniparser/parser/ParserTest.java
+++ src/test/java/com/kramermp/iniparser/parser/ParserTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
     String sampleKey = "[SampleKey]";
+    String sampleBadKey1  = "SampleBadKey1]";
+    String sampleBadKey2  = "[SampleBadKey2";
     String sampleLine = "SampleLine=This";
     String sampleComment = "#SampleComment";
     String emptyLine = "";
@@ -38,6 +40,16 @@ class ParserTest {
     void isKeyTrue() {
         assertTrue(Parser.isKey(sampleKey));
     }
+
+    @Test
+    void isKeyFalseBadKey1() {
+        assertFalse(Parser.isKey(sampleBadKey1));
+    }
+    @Test
+    void isKeyFalseBadKey2() {
+        assertFalse(Parser.isKey(sampleBadKey2));
+    }
+
 
     @Test 
     void isKeyFalseLine() {
